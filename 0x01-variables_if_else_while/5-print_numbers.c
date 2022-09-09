d25cf0ea067c6c8bfa7adaa03a524a69e91fef81
Precious <ikpaprecious2@gmail.com>
2022-09-09 11:43:35 -0700

    variable

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,7 +10,9 @@ int main(void)
 	int digit;
 
 	for (digit = 0; digit <= 10; digit++)
+	{
 		printf("%d", digit);
 		putchar('\n');
+	}
 		return (0);
 }
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,7 +10,9 @@ int main(void)
 	int digit;
 
 	for (digit = 0; digit < 10; digit++)
+	{
 		putchar(digit + '0');
 		putchar('\n');
+	}
 		return (0);
 }
