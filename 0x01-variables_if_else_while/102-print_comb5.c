c498460a02329a8814e5bf5f0fe9e14ffe3b139b
Precious <ikpaprecious2@gmail.com>
2022-09-09 15:58:24 -0700

    betty head

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+*main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int ten;
+	int one;
+	int a;
+	int b;
+
+	for (ten = '0'; ten <= '9'; ten++)
+	{
+		for (one = '0'; one <= '9'; one++)
+		{
+			for (a = ten + 1; a <= '9'; a++)
+			{
+				for (b = one + 1; b <= '9'; b++)
+				{
+					putchar(ten);
+					putchar(one);
+					putchar(' ');
+					putchar(a);
+					putchar(b);
+					if (!((ten == '9' && one == '8') && (a == '9' && b == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				b = '0';
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
