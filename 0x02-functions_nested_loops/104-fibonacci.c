0f2cc7acca81e8fb94d01c46ada89e13907d2ffa
Precious <ikpaprecious2@gmail.com>
2022-09-13 15:27:52 -0700

    adv5

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -37,7 +37,9 @@ int main(void)
 		if (count != 98)
 			printf(", ");
 		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;												fib2_half1 = half1;
+		fib1_half2 =
+		fib2_half2;
+		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
 	printf("\n");
