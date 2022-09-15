3b750737ff92a5bbfd01876203a78bbf93404cb3
Precious <ikpaprecious2@gmail.com>
2022-09-15 13:01:45 -0700

    more functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,15 +10,16 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
-		if (i % 15 == 0)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (i % 5 == 0)
+		else if ((i % 5) == 0)
 		{
 			printf("Buzz");
 		}
-		else if (i % 3 == 0)
+		else if ((i % 3) == 0)
 		{
 			printf("Fizz");
 		}
@@ -26,7 +27,10 @@ int main(void)
 		{
 			printf("%d", i);
 		}
-	printf("Buzz");
+		if (i == 100)
+			continue
+		printf(" ");
+	}
 	printf("\n");
 	return (0);
 }
