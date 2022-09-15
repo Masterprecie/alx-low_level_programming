13b3e58253560479dc68e52ec95d69d7992b2685
Precious <ikpaprecious2@gmail.com>
2022-09-15 09:03:00 -0700

    more functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,9 +5,9 @@
 */
 void more_numbers(void)
 {
-	int num, num_count;
+	int num, count;
 
-	for (num_count = 0; num_count <= 9; num_count++)
+	for (count = 0; count <= 9; count++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
