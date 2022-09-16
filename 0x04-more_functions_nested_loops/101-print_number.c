21dbabfb41d9db2b995037250056fe4d856ac9be
Precious <ikpaprecious2@gmail.com>
2022-09-15 21:16:53 -0700

    more function

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,43 +4,33 @@
 /**
 * print_number - checks for checks for a digit (0 through 9)
 * @n: n - variable.
-* Return: Always 0.
+* Return: void
 */
+
 void print_number(int n)
 {
-	unsigned int z;
+	int copy, nth, size = 1, ones = n % 10;
 
-	int m, b;
-
-	b = 10;
-	if (n < 10 && n >= 0)
+	n /= 10;
+	copy = n;
+	if (ones < 0)
 	{
-		_putchar (n + '0');
-	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
+		ones *= -1, copy *= -1, n *= -1;
 		_putchar('-');
-		_putchar (n + '0');
 	}
-	else
+	if (copy > 0)
 	{
-	if (n < 0)
-	{
-		n = n * -1
-			_putchar ('-');
+		while (copy / 10 != 0)
+		{
+			copy /= 10, size *= 10;
+		}
+		while (size > 0)
+		{
+			nth = n / size;
+			_putchar('0' + nth);
+			n -= nth * size;
+			size /= 10;
+		}
 	}
-	z = n;
-	while (z / b > 9)
-	{
-		b = b * 10;
-	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		_putchar (m + '0');
-		b = b / 10;
-	}
-	}
+	_putchar('0' + ones);
 }
