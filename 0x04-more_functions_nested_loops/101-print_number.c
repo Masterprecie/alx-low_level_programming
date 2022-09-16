7b0eacd980f353fc89c94c7695b2403198da202d
Precious <ikpaprecious2@gmail.com>
2022-09-15 20:56:29 -0700

    more function

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,39 @@
 */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int z;
 
+	int m, b;
+
+	b = 10;
+	if (n < 10 && n >= 0)
+	{
+		_putchar (n + '0');
+	}
+	else if (n > -10 && n < 0)
+	{
+		n = n - 2 * n;
+		_putchar('-');
+		_putchar (n + '0');
+	}
+	else
+	{
 	if (n < 0)
 	{
-		_putchar('-');
-		num = -num;
+		n = n * -1
+			_putchar ('-');
 	}
-	if ((num / 10) > 0)
-		print_number(num / 10);
-	_putchar((num % 10) + '0');
+	z = n;
+	while (z / b > 9)
+	{
+		b = b * 10;
+	}
+	while (b > 0)
+	{
+		m = z / b;
+		z = z % b;
+		_putchar (m + '0');
+		b = b / 10;
+	}
+	}
 }
