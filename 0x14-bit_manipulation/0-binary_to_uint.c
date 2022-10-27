9dc33d4d40854c5245a20a40833e06cb534a9756
Precious <ikpaprecious2@gmail.com>
2022-10-27 21:00:57 +0100

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,34 @@
+#include <stddef.h>
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
+ * binary_to_uint - converts a bit string to unsigned int
  *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+ * @b: string to convert
+ *
+ * Return: decimal unsigned int value
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int n;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
-
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	for (n = 0; *b != 0; b++)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (*b == '0')
+		{
+			n = n << 1;
+		}
+		else if (*b == '1')
+		{
+			n = n << 1;
+			n++;
+		}
+		else
+		{
 			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		}
 	}
-
-	return (num);
+	return (n);
 }
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -2,5 +2,5 @@ Learning Objectives
 At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
 
 General
-When and why using linked lists vs arrays
-How to build and use linked lists
+Look for the right source of information without too much help
+How to manipulate bits and use bitwise operators
